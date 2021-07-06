72810f903478145b8b2c2e497d4fae7d2ca1a415
mistico0121 <mrschudeck@uc.cl>
2021-07-05 22:58:13 -0400

    testing deployment

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
-# Cloudnet-Backoffice
+# iic2173-e0-mistico0121
 
-Comandos para iniciar aplicación de videollamada:
+Está hecho en Django, Gunicorn, Postgres con motor web nginx como el proxy inverso
 
-`
-npm run devStart
-`
+### Local
 
-`
-peerjs --port 3001 
-`
+Usar `sudo docker-compose -f docker-compose-dev.yml up --build -d` para levantar la app en un daemon thread en local. Ejecutar `sudo docker-compose exec web python manage.py migrate --noinput` para migrar la base de datos.
+
+Si ocurre un error, hacer `sudo docker-compose -f docker-compose-dev.yml down -v` para bajar la app y los volumenes, e intentar nuevamente
+
+Luego metan todo lo que necesiten a los templates de la app. Esta ya está arriba y funcionando, solo debiera bastar hacer git pull y migrar cuando agreguen sus views
+
+
+### Docker-compose
+
+Pueden verse los detalles de los 3 requisitos en el archivo docker-compose. La aplicación se encuentra en un daemon thread y pueden bajarla del server, y luego volverla a subir con "sudo docker-compose up". Si quisieran bajar los volúmenes, para volver a subirlos se debe hacer `sudo docker-compose -d --build`, `sudo docker-compose exec web python manage.py migrate --noinput`, `sudo ./init-letsencrypt.sh` (script para obtener permisos SSL actualizados, si no se puede ejecutar se debe correr `chmod +x init-letsencrypt.sh`) y luego `screen` y adentro `sudo docker-compose up`. Así lo dejé corriendo en el servidor EC2. 
+
+Los detalles del proxy inverso se pueden ver tanto en docker-compose.yml como en nginx/conf.d con sus puertos correspondientes.
---
+++ app/videocalls/views.py
@@ -0,0 +1,94 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Videocall, Survey
+from .forms import SurveyForm
+import requests
+import environ
+from time import sleep
+from threading import Thread
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+
+#VIDEOLLAMADAS
+
+@login_required
+def videocall_index_view(request):
+    queryset = Videocall.objects.all()
+    context = {
+        'object_list': queryset
+    }
+    return render(request, "videocall/index.html", context)
+
+def videocall_create():
+    env = environ.Env()
+    environ.Env.read_env()
+    videocall_url = env('VIDEOCALL_URL')
+    sleep(1)
+    req = requests.get(f'{videocall_url}/API')
+    link = f'{videocall_url}/' + str(req.content).replace("b", "", 1).strip("'")
+    new_call = Videocall(url=link)
+    new_call.save()
+
+def videocall_middleware(request):
+    env = environ.Env()
+    environ.Env.read_env()
+    videocall_url = env('VIDEOCALL_URL')
+    create = Thread(target=videocall_create)
+    create.start()
+
+    return redirect(videocall_url)
+
+def videocall_delete(request, data):
+    obj = Videocall.objects.get(pk=data)
+    url = obj.url
+    obj.delete()
+    return redirect(url)
+
+def ajax_get_view(request):
+    queryset = Videocall.objects.all()
+    context = {
+        'object_list': queryset
+    }
+    if request.is_ajax():
+        ids = []
+        for videocall in queryset:
+            ids.append(videocall.id)
+        data = {
+            'ids': ids
+        }
+        return JsonResponse(data)
+    return render(request, "videocall/index.html", context)
+
+#ENCUESTAS
+
+@login_required
+def surveys(request):
+    queryset = Survey.objects.all()[::-1][:10]
+    suma_calidad = 0
+    suma_utilidad = 0
+    for i in queryset:
+        suma_calidad += i.quality_rating
+        suma_utilidad += i.usefullness_rating
+    if len(queryset) != 0:    
+        context = {
+            'object_list': queryset,
+            'mean_quality_rating': round(suma_calidad/len(queryset), 2),
+            'mean_usefullness_rating': round(suma_utilidad/len(queryset), 2)
+        }
+    else:
+        context = {
+            'object_list': queryset,
+            'mean_quality_rating': 0,
+            'mean_usefullness_rating': 0
+        }
+
+    return render(request, "videocall/surveys.html", context)
+
+def survey_create(request):
+    form = SurveyForm(request.POST or None)
+    if form.is_valid():
+        fcc_form = form.save(commit=True)
+        return render(request, "home.html", {})
+    context = {
+        'form': form
+    }
+    return render(request, "videocall/new.html", context)
